1923461ae4c0e7219d9bdafcf91293d7fd4a39e6
laura-schoenherr <lauraschoenherr@outlook.de>
2024-05-25 00:30:08 +0200

    add axios

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+export default 
+
+defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/fonts.css'],  
-})
+  css: ['~/assets/css/fonts.css'],
+  modules: [
+    '@nuxtjs/axios',
+  ],
+  axios: {
+    // Replace with your backend API URL
+    baseURL: 'http://localhost/path_to_your_php_script',
+  },
+}
+)
+
+
